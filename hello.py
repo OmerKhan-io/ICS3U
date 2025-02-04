4ed78f3c5367ad271161f2aaae996d61b5bdf67f
OmerKhan-io <753619@pdsb.net>
2025-02-04 12:07:47 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = "Leslie"
+print("Hello", name)
+# This is because name is the variable that holds the character Leslie. The comma is a place holder not a character that will be printed
+
+#Modify
+name = "Khan"
+print("Hello,", name)
+# my name is printed
