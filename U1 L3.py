3ecad0ce46fa252f842e81043894d61687e9e601
OmerKhan-io <753619@pdsb.net>
2025-02-06 12:37:04 -0500

    Update and rename Lesson 3.py to U1 L3.py

---
+++ U1 L3.py
@@ -0,0 +1,20 @@
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute ",12) # it will print "It's possible Sanjay can distribute 12" (with one space)
+print("cookies for each bowl.") #next line
+#task B
+cookies = 144
+bowl = 6
+print("Sanjay has", bowl, "bowls for", cookies, "cookies")
+#task C
+print("The number of cookies per bowl is:", cookies/bowl)
+      
+# output text and numbers
+print("There are ", 28, " people in this room.");
+print("Your total money spent is: ", (2.75+9.32+11.91)) # The integers are added up and then the sum is printed
+print("There are %d people in this room." % 28) # %d is a place holder for the integer "28".
+print("Pi is equal to %f" % 3.1416) # %f is a place holder for the the type double "3.1416" 
+print("%s's final score is %d" % ("Joe", 215)) # %s holds the value for the string "Joe". %d is a place holder for the integer "215".
+print("Total money spent is: %.2f" % (2.75+9.32+11.91)) # %.2f restricts the decimal to 2 decimal places
+print("The area of a circle of radius %d is %f" % (4, 3.1416*4*4)) # prints out the area of a circle of a radius. %d holds the value of 4, and %f holds the value of 3.1416*4*4 
+
+
