4397b5605eee53eaba320cd700ee171dc737db37
OmerKhan-io <753619@pdsb.net>
2025-02-10 12:55:33 -0500

    Create U1L6.py

---
+++ U1L6.py
@@ -0,0 +1,10 @@
+BirthYear = input("Enter your birth year: ")
+Age = input("Enter your age: ")
+BirthYear = int(BirthYear)
+Age = int(Age)
+BirthYear += 5
+BirthYear *=50
+BirhYear = BirthYear + Age
+BirthYear -= 250
+BirthYear /=100
+print(BirthYear)
