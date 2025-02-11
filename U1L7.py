0abc6a6ab4ca6f041242c3567aad6a464da83fef
OmerKhan-io <753619@pdsb.net>
2025-02-11 14:27:50 -0500

    Create U1L7.py

---
+++ U1L7.py
@@ -0,0 +1,64 @@
+# I predict that it would ask the user for the x and y value, then it will check if there have a common factor. 
+# If it does it will print the value of x and y, if the don't have a common factor, it will not print the final print statement. In this case, it will stop at the Yes! 3 is a factor of 6
+
+
+import math
+
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+print("Now deciding if", y, "is a factor of", x, "...")
+rem = x % y
+if rem == 0:
+    print("Yes!", y, "is a factor of", x)
+
+#Modify 1 and 2
+import math
+
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+print("Now deciding if", y, "is a factor of", x, "...")
+
+if y != 0:
+    print("Yes!", y, "is a factor of", x)
+else:
+      print("Do not enter a zero")
+#modify 1 again
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+print("Now deciding if", y, "is a factor of", x, "...")
+if y!=0:
+      print("Do not enter a zero")
+rem = x % y
+if rem == 0: 
+  print("Yes!", y, "is a factor of", x)
+else:
+    print("No!", y, "is a factor of", x)
+
+
+#modify 3
+# I predict that it would ask the user for the x and y value, then it will check if there have a common factor. If it does it will print the value of x and y, if the don't have a common factor 
+import math
+
+x = input("Please input a whole number between 1 and 20: ")
+x = int(x)
+y = input("Please input another whole number between 1 and 20: ")
+y = int(y)
+if (x >= 0) and (x <= 20):
+  print( x, "is between 1 and 20")
+else: 
+  print("No!", x, "is out of range")
+if (y >= 0) and (y <= 20):
+  print(x, "is between 1 and 20")
+else: 
+  print("No!", y, "is out of range")
+rem = x % y
+if rem ==0:
+    print("Yes!", y, "is a factor of", x)
+else:
+      print(y, "is not a factor of", x)
