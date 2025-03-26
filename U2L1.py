9885fc8327a932901d621b0fea4c98423d79b49b
OmerKhan-io <753619@pdsb.net>
2025-03-26 13:01:54 -0400

    Update U2L1.py

--- U2L1.py
+++ U2L1.py
@@ -34,3 +34,20 @@ elif (ch == "C"):
     ("I prefer cherries")
 else:
     print("Error...")
+
+# Modify 2
+
+choice = int(input("Enter a number between 1 and 100"))
+
+if (choice>=80) and (choice <=100):
+  print("A")
+elif (choice>=70) and (choice<=79):
+  print("B")
+elif (choice>=60) and(choice<=69):
+  print("C")
+elif (choice>=50) and (choice<=59):
+  print("D")
+elif (choice<50):
+  print("F")
+else:
+  print("Not a valid number")
