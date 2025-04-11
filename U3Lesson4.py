e2ac114b856a9b5a7a3d48863bada88ad75e0ea7
OmerKhan-io <753619@pdsb.net>
2025-04-11 00:12:12 -0400

    Create U3Lesson4.py

---
+++ U3Lesson4.py
@@ -0,0 +1,28 @@
+ar2 = [[3, 4, 1, 2, 6],
+      [9, 2, 3, 7, 5],
+      [4, 2, 1, 0, 3]]
+for i in range(len(ar2)):
+  ar3 = ar2[i]
+  for j in range(len(ar3)):
+      print(ar3[j], end=" ")
+  print() # PREDICT A 
+#Prints the values of each row printed in each line follow by spaces after each character
+
+print(ar2) # PREDICT B
+#Prints the entire 2-D array as a list of list, exactly how it was stored in Python
+
+
+# Modify 
+ar2 = [[3, 4, 1, 2, 6],
+      [9, 2, 3, 7, 5],
+      [4, 2, 1, 0, 3]]
+sum_list = [] # This will store total number of each row "scores"
+              # One at a time
+sun_lists = []
+for i in range(len(ar2)):
+  #The len for the array is 3, so it will 
+  ar3 = ar2[i]
+  sum_list = sum(ar3)
+  sum_lists.append(sum_list)
+  print(sun_lists)
+  
