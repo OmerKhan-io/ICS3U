f763eb9c8732d28b9f2512ebbf6c373f33ec6eda
OmerKhan-io <753619@pdsb.net>
2025-04-14 13:07:20 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,1 +1,8 @@
+print("Welcome to the Palindrome Program")
+ar = [ "noon", "racecar", "civic", "madam", "desk", "kayak", "soccer", "level", "school", "radar"]
 
+#Create loops that c=goes through each word
+for word in ar:
+  letter_match = 0
+  letter_positon =  0
+  midway = len(word) //2
