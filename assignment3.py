837678312b815e6cb6860bc8b7fb1d4a700c9243
OmerKhan-io <753619@pdsb.net>
2025-04-15 14:37:32 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -6,3 +6,13 @@ for word in ar:
   letter_match = 0
   letter_positon =  0
   midway = len(word) //2
+while letter_position < halfway: 
+  if word[letter_position] == word[len(word) - 1 - letter_position]
+  #Checks if the letter from the beginning mtahces the letter from the end of the word
+  letter_math +=1 
+  #if letter matches; then increase the count to next set of letters
+  
+  
+  
+  
+  print( 
