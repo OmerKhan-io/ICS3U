f6279025f7ea5fec86612a9fcbf6b4f59f2420e2
OmerKhan-io <753619@pdsb.net>
2025-04-16 16:09:05 -0400

    Create U2L5.py

---
+++ U2L5.py
@@ -0,0 +1,9 @@
+####1 
+
+s = "#"
+spsces = ""
+max = 9 #max number of characters
+for i in range (1, 10, +2):
+  spaces =" "*((max-i)//2) #generating correct number of spaces
+  print(spaces + i*s) #added the spaces 
+
