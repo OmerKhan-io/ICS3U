072426c68869629b88e7bc02eac7edb71d456e15
OmerKhan-io <753619@pdsb.net>
2025-04-25 19:37:22 -0600

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -6,7 +6,7 @@
   Description : A program that determines whether the words in the given list is a palindrome or not
 VARIABLE DICTIONARY
   ar(list) = a list of words that will be checked to see if they are palindromes
-  word(string) = The current word being check in teh for loop
+  word(str) = The current word being check in teh for loop
   letter_match(int) = Counts how many letters (from start and end) are matching
   letter_positon(int) = the index used to move from the beginning of the word to its centre
   midway(int) = the halfway point of the word. The point to stop checking for letters in a word
@@ -31,7 +31,7 @@ for word in ar:
       #Move to the next letter position
   
   #After checking all letters upto the midway point
-  #Checks to see if all the compared letters matched, then the word is a palindrome
+  #Checks to see If all mirrored letters matched up to midway, it's a palindrome
   if letter_match == midway:
    
     print(word, "is a palindrome")
