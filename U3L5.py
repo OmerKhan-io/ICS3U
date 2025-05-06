4a43071b2e91b190e524a72367b3473b48538100
OmerKhan-io <753619@pdsb.net>
2025-05-06 12:45:56 -0400

    Create U3L5.py

---
+++ U3L5.py
@@ -0,0 +1,16 @@
+# I predict that the function will add two numbers and then the will be printed outside the function
+def add(a: float, b: float) -> float:
+   sum = a + b
+   return sum
+sum = add(7.0, 2.0)
+print(sum) #this will print output as a float data type
+
+#Modify 2
+def a_abs (a):
+  if a < 0:
+    return a*-1
+  else:
+    return a*-1
+    
+print(abs(-5.2))
+
