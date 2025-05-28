ed98643db468c23ed676fc28cb3bd93aff1d490c
OmerKhan-io <753619@pdsb.net>
2025-05-28 13:08:35 -0400

    Update assignment4new.py

--- assignment4new.py
+++ assignment4new.py
@@ -1,6 +1,34 @@
-#Creating an array to store each line of the file as values
+"""
+Author : Omer Khan
+Student number: 753619
+Course code: ICS3U
+Revision date : May 17, 2025
+Program : Wordle Database Search
+Description : A program the searches for words and date entered by the users in the Wordle Database File 
+    
+VARIABLE DICTIONARY :
+  arr (list) =  stores each line of the file as an array list
+  arr_date (list) = stores every line of the dates in the file as an array list
+  arr_word (list) = stores all the words from the file 
+  arr_months (list) = this array contains all the months of the year
+  fh (file object) = File handle used to open/read the "wordle (1).dat" file
+  line (str) = Stores one line of text read from the file, stripped of whitespace
+  month (str) = stores the three letter abbreviation of the month
+  day (str) = stores the day portion of a date
+  year (str) = stores the year portion of a date
+  date (int) = temporary variable wehn looping through arr_date
+  i (int) = Loop index variable for going through the arr list
+  user_input_word (str) = the user input string for searching a word in the file 
+  user_input_date (int) = 	the user input integer date in YYYYMMDD format, used to find the corresponding word on that date
+  user_choice (str) = User input to choose search mode: "w" for word search, "d" for date search
+  w (str) = user input word for search 
+  user_year (int) = 	User input year for date search
+  user_month 
+"""
+
+#Creating an empty array to store each line of the file as values
 arr = []
-#Creating array that holds date in number format from the file
+#Creating an array that holds date in number format from the file
 arr_date = []
 arr_word= []
 arr_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -26,7 +54,7 @@ def merge_date(month,day,year):
   # Turns year into YYYY0000
   year = int(year) * 10000
   # Turns month name to MM00
-  month = (arr_months.index(month) + 1) * 100  
+  month = (arr_months.index(month)+1) * 100  
   day = int(day)
   # Now combines the year, month, and the day 
   return year + month + day  
@@ -72,11 +100,11 @@ elif user_choice == "d":
    #The user enters the year of the date 
    user_year = int(input("Enter the year: "))
    #The user enters the month of the date
-   user_month =int(input("Enter the month: ")) 
+   user_month =input("Enter the month (Month name abbriviation): ").capitalize()
    #The user enters the day of the date
    user_day = int(input("Enter the day: "))
    if 20210619 <= merge_date(user_month, user_day, user_year) <= 20240421:
-      print("The word you entered on", merge_date(user_month, user_day, user_year), "was", merge_date(user_month, user_day, user_year))
+      print("The word you entered on", merge_date(user_month, user_day, user_year), "was", date_match(merge_date(user_month, user_day, user_year)))
       # If date too early
    elif 20210619 > merge_date(user_month, user_day, user_year): 
     # Prints given date is too early
